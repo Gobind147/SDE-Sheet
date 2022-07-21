8cbfc2aebd3f5e65f221c03c88535a7673b054bd
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-07-21 19:01:48 +0530

    Update 1. LeaderinArray.cpp

--- Arrays/1. LeaderinArray.cpp
+++ Arrays/1. LeaderinArray.cpp
@@ -1,7 +1,7 @@
-https://practice.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1
-Given an array A of positive integers. Your task is to find the leaders in the array.
-An element of array is leader if it is greater than or equal to all the elements to its right side.
-The rightmost element is always a leader.
+//https://practice.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1
+//Given an array A of positive integers. Your task is to find the leaders in the array.
+//An element of array is leader if it is greater than or equal to all the elements to its right side.
+//The rightmost element is always a leader.
 
 
 #include <iostream>
