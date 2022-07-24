6aed13ba987e9b8c11514199a817442c19f0fef5
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-07-24 22:47:39 +0530

    Create 1. FirstRepeatedCharacter.cpp

---
+++ String/1. FirstRepeatedCharacter.cpp
@@ -0,0 +1,36 @@
+//Find first repeated character
+//Given a string, find the first repeated character in it. 
+//We need to find the character that occurs more than once and whose index of second occurrence is smallest
+//https://practice.geeksforgeeks.org/problems/find-first-repeated-character/0
+//https://www.geeksforgeeks.org/find-the-first-repeated-character-in-a-string/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        string s;
+        cin>>s;
+        unordered_set<char> h;
+        for(int i=0;i<h.size();i++)
+        {
+            char c = s[i];
+            if (h.find(c) != h.end())
+            {
+            cout<< c;
+            return 0;
+        }
+            else 
+            h.insert(c);
+        }
+        
+    }
+
+    return 0;
+}
